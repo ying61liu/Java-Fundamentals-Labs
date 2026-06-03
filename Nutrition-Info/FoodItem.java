97fa0d02d1f69e970bd87f491cae36352b0aba40
ying61liu <ying61liu@gmail.com>
2026-06-03 16:36:17 +0000

    uploads a program that outputs nutritional info of food.

---
+++ Nutrition-Info/FoodItem.java
@@ -0,0 +1,51 @@
+public class FoodItem {
+   private String name;
+   private double fat;
+   private double carbs;
+   private double protein;
+   
+   // TODO: Define default constructor
+   public FoodItem() {
+      name = "Water";
+      fat = 0.0;
+      carbs = 0.0;
+      protein = 0.0;
+   }
+   
+   // TODO: Define second constructor with parameters to initialize private fields (name, fat, carbs, protein)
+   public FoodItem(String inputName, double inputFat, double inputCarbs, double inputProtein) {
+      name = inputName;
+      fat = inputFat;
+      carbs = inputCarbs;
+      protein = inputProtein;
+   }
+      
+   public String getName() {
+      return name;
+   }
+   
+   public double getFat() {
+      return fat;
+   }
+   
+   public double getCarbs() {
+      return carbs;
+   }
+   
+   public double getProtein() {
+      return protein;
+   }
+   
+   public double getCalories(double numServings) {
+      // Calorie formula
+      double calories = ((fat * 9) + (carbs * 4) + (protein * 4)) * numServings;
+      return calories;
+   }
+   
+   public void printInfo() {
+      System.out.println("Nutritional information per serving of " + name + ":");
+      System.out.printf("  Fat: %.2f g\n", fat);
+      System.out.printf("  Carbohydrates: %.2f g\n", carbs);
+      System.out.printf("  Protein: %.2f g\n", protein);
+   }
+}

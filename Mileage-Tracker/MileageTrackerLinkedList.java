579f6c7631eb6c3bea1000949051509a63a7b598
ying61liu <ying61liu@gmail.com>
2026-06-09 18:56:23 +0000

    uploads a program that creates and prints a linked list to track mileage for a runner.

---
+++ Mileage-Tracker/MileageTrackerLinkedList.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class MileageTrackerLinkedList {
+   public static void main (String[] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      // References for MileageTrackerNode objects
+      MileageTrackerNode headNode;                                           
+      MileageTrackerNode currNode;
+      MileageTrackerNode lastNode;
+
+      double miles;
+      String date;
+      int i;
+
+      // Front of nodes list                                                                         
+      headNode = new MileageTrackerNode();
+      lastNode = headNode;
+
+      // TODO: Scan the number of nodes
+      int numNodes = scnr.nextInt();
+      
+      // TODO: For the scanned number of nodes, scan
+      //       in data and insert into the linked list
+      for (i = 0; i < numNodes; i++) {
+         miles = scnr.nextDouble();
+         date = scnr.next();
+         
+         currNode = new MileageTrackerNode(miles, date);
+         lastNode.insertAfter(currNode);
+         lastNode = currNode;
+      }
+
+      // TODO: Call the printNodeData() method 
+      //       to print the entire linked list
+      currNode = headNode.getNext();
+      
+      while (currNode != null) {
+         currNode.printNodeData();
+         currNode = currNode.getNext();
+      }
+   }
+}
---
+++ Mileage-Tracker/README.md
@@ -0,0 +1,5 @@
+Given the MileageTrackerNode class, complete main() in the MileageTrackerLinkedList class 
+to insert nodes into a linked list (using the insertAfter() method). 
+
+The first user-input value is the number of nodes in the linked list. 
+Use the printNodeData() method to print the entire linked list. DO NOT print the dummy head node.
